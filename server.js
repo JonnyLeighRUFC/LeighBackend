f20dae77722857911ee7ff1d78ac421b70214862
JonnyLeighRUFC <74961171+JonnyLeighRUFC@users.noreply.github.com>
2020-12-09 17:57:07 +0000

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobil
 mobileApp.tables.add('Post'); // Create a table for 'login' with default settings
 mobileApp.tables.add('Users'); // Create a table for 'Users' with default settings
 mobileApp.tables.add('Newsletter'); // Create a table for 'Newsletter' with default settings
+mobileApp.tables.add('Orders'); // Create a table for 'Orders' with default settings
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
